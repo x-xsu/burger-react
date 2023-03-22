a344f8b542558a289826785ff77238b232664d82
Ksenia Vasileva <k.vasilyeva@mediasoft.team>
2023-03-22 15:51:45 +0400

    start project

---
+++ src/components/Container/Container.jsx
@@ -0,0 +1,8 @@
+import style from "./Container.module.css";
+import classNames from "classnames";
+
+export const Container = ({ children, className }) => (
+  <div className={classNames(style.container, className)}>
+    {children}
+  </div>
+);
